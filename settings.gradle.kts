aef40b6e58365587899d1b31c4c8caf95617be58
gyeoul <yuriasoliven09@gmail.com>
2026-04-27 12:46:45 +0800

    Fix: Re-add toolchain resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,10 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 dependencyResolutionManagement {
