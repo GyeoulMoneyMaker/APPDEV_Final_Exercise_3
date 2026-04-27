d3acac5555f66a7357c5364265b214e59fc209ad
gyeoul <yuriasoliven09@gmail.com>
2026-04-27 12:15:24 +0800

    Fix: Downgrade SDK versions and update Java compatibility to resolve build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.crud_basedappdevelopment"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.crud_basedappdevelopment"
-        minSdk = 28
-        targetSdk = 36
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -30,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
